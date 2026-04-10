505839b804104ad4b768df5765df533d9a9def9b
nezukox01-hash <nezukox01@gmail.com>
2026-04-11 01:19:59 +0600

    Create reminder_screen.dart

---
+++ ts_reminder/lib/screens/reminder_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ReminderScreen extends StatelessWidget {
+  const ReminderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Reminder')),
+      body: const Center(child: Text('Reminder Screen')),
+    );
+  }
+}
