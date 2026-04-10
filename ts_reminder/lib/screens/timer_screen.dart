9438e8eb52b9633384dd704b064803b98388cc2a
nezukox01-hash <nezukox01@gmail.com>
2026-04-11 01:14:53 +0600

    Update timer_screen.dart

--- ts_reminder/lib/screens/timer_screen.dart
+++ ts_reminder/lib/screens/timer_screen.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class TimerScreen extends StatelessWidget {
+  const TimerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Timer')),
+      body: const Center(child: Text('Timer Screen')),
+    );
+  }
+}
