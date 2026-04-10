fdb373cc6596005d7b9a7d17d5675eaf2afa2e59
nezukox01-hash <nezukox01@gmail.com>
2026-04-10 23:56:00 +0600

    Create bottom_nav_bar.dart

---
+++ ts_reminder/lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import '../utils/colors.dart';
+
+class AppBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const AppBottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 86,
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      decoration: const BoxDecoration(
+        color: AppColors.navBackground,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _item(
+            index: 0,
+            icon: Icons.timer_outlined,
+            label: 'Timer',
+          ),
+          _item(
+            index: 1,
+            icon: Icons.bar_chart_outlined,
+            label: 'Stats',
+          ),
+          GestureDetector(
+            onTap: () => onTap(2),
+            child: Container(
+              height: 72,
+              width: 72,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.white.withOpacity(0.15),
+                    blurRadius: 16,
+                    spreadRadius: 2,
+                  ),
+                ],
+              ),
+              child: const Icon(
+                Icons.home_rounded,
+                color: AppColors.background,
+                size: 34,
+              ),
+            ),
+          ),
+          _item(
+            index: 3,
+            icon: Icons.description_outlined,
+            label: 'Report',
+          ),
+          _item(
+            index: 4,
+            icon: Icons.note_outlined,
+            label: 'Notes',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _item({
+    required int index,
+    required IconData icon,
+    required String label,
+  }) {
+    final bool selected = currentIndex == index;
+
+    return GestureDetector(
+      onTap: () => onTap(index),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: selected ? AppColors.navSelected : AppColors.navUnselected,
+            size: 26,
+          ),
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(
+              color: selected ? AppColors.navSelected : AppColors.navUnselected,
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
