f80f501cfb2d45cd994d44279fbad5cc0d52306b
nezukox01-hash <nezukox01@gmail.com>
2026-04-16 02:57:25 +0600

    Update main.dart

--- ts_reminder/lib/main.dart
+++ ts_reminder/lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
+
 import 'screens/home_screen.dart';
+import 'services/midnight_alarm_service.dart';
 import 'services/task_reminder_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await AndroidAlarmManager.initialize();
   await TaskReminderService.init();
+  await MidnightAlarmService.scheduleNextMidnightAlarm();
+
   runApp(const TSReminderApp());
 }
 
